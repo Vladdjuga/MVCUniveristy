960c8235f3fd36db07a7900d743d9ca6c8f1ef74
Денис Свиридюк <densvir33@gmail.com>
2021-02-18 18:12:13 +0200

    sdfosigbiq

---
+++ poopi/Areas/Student/Controllers/StudentPanelController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace poopi.Areas.Student.Controllers
+{
+    public class StudentPanelController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
